da7af7f4abae93f59be56e317c45c98c76f8ab2f
peterenenche <itcrusaders19@gmail.com>
2021-12-09 12:55:05 +0100

    Initial commit

---
+++ app/src/main/java/com/techvibesltd/forcollins/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.techvibesltd.forcollins
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.web_pos)
+    }
+}
